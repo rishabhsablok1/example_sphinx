b97750129fe438f8adb309aa2d0bda052aaaeb75
rishabhsablok1 <rishabhsablok1@gmail.com>
2021-05-03 19:06:19 -0400

    Adding functions

---
+++ example.py
@@ -0,0 +1,20 @@
+def function1(arg1, arg2):
+    """Sets first argument equal to second
+
+    Args:
+        arg1: Some values
+        arg2: Some values
+
+    Return:
+        argument 1
+    """
+    arg1 = arg2
+    return arg1
+
+def function2():
+    """Function with no arguments
+    
+    Return:
+        None
+    """
+    return None
